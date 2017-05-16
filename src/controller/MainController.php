91dbb66e84377ca42faa6657e60fc6f56061feb9
rmarru <ls31252@salleurl.edu>
2017-05-17 00:26:19 +0200

    Removes funcionen!!

--- src/controller/MainController.php
+++ src/controller/MainController.php
@@ -367,23 +367,27 @@ class MainController {
 
     public function removeImage (Application $app, $idImg) {
         $userController = new DatabaseController();
+        $img = $userController->getImageAction($app, $idImg);
 
-       /* if ($userController->getNotification($app, $idImg, )) {
-            $userController->updateNotificationUser($app, $userController->getAction($app, $app['session']->get('name'))['id'], $userController->getNotificationNum($app, $idImg), 0);
-        }*/
+        if ($userController->getNotificationNum($app, $idImg)) {
+            $userController->updateNotificationUser($app, $img['user_id'], $userController->getNotificationNum($app, $idImg), 0);
+        }
 
         $userController->deleteImageAction($app, $idImg);
-        header('Location: ' . '/user/' . $this->user['id'], true, 303);
+        header('Location: ' . '/user/' . $img['user_id'], true, 303);
         die();
     }
 
     public function removeComment (Application $app, $idComment) {
         $userController = new DatabaseController();
+        $com = $userController->getCommentId($app, $idComment);
 
-        $userController->updateNotificationUser($app, $userController->getAction($app, $app['session']->get('name'))['id'], 1, 0);
+        if ($userController->getNumComment($app, $idComment)) {
+            $userController->updateNotificationUser($app, $com['user_id'], $userController->getNumComment($app, $idComment), 0);
+        }
 
         $userController->deleteCommentAction($app, $idComment);
-        header('Location: ' . '/user/' . $this->user['id'], true, 303);
+        header('Location: ' . '/comment-list/' . $com['user_id'], true, 303);
         die();
     }
 
