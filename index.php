d940bc990782cc0b3c4a3b074074ec88feb15a2c
Togekiss <martazapateroweb@gmail.com>
2024-08-09 21:54:14 +0200

    moving index to root

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marta
+ * Date: 29/03/2017
+ * Time: 19:13
+ */
+
+ini_set('display_errors', 1);
+require_once __DIR__.'/vendor/autoload.php';
+$app = require __DIR__.'/app/app.php';
+require __DIR__.'/app/config/prod.php';
+require __DIR__.'/app/config/routes.php';
+$app['debug'] = true;
+
+$app->run();
