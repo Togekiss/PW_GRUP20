fe15ee98e4c76f14b8245e60e966bd9ec1f709a4
Togekiss <martazapateroweb@gmail.com>
2024-08-10 16:50:02 +0200

    remove ini display errors

--- index.php
+++ index.php
@@ -7,14 +7,18 @@
  */
 
 ob_start();
-session_start();
 
 error_reporting(E_ALL & ~E_DEPRECATED);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
+
 require_once __DIR__.'/vendor/autoload.php';
 $app = require __DIR__.'/app/app.php';
 require __DIR__.'/app/config/prod.php';
 require __DIR__.'/app/config/routes.php';
+
+// Call session_start() after the configuration settings
+session_start();
+
 $app['debug'] = false;
 
 // Load and execute the SQL schema file
