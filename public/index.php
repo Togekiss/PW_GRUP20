26c8dea81df169565e979f5a8a6abbefd90fb227
Togekiss <martazapateroweb@gmail.com>
2024-08-11 17:12:13 +0200

    activating error debugging

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
 ob_start();
 
 error_reporting(E_ALL & ~E_DEPRECATED);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 
 require_once __DIR__.'/../vendor/autoload.php';
 $app = require __DIR__.'/../app/app.php';
@@ -19,7 +19,7 @@ require __DIR__.'/../app/config/routes.php';
 // Call session_start() after the configuration settings
 session_start();
 
-$app['debug'] = false;
+$app['debug'] = true;
 
 // Load and execute the SQL schema file
 $schemaFile = __DIR__ . '/../app/config/schema.sql';
